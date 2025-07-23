0a549fd84f54eea5e7b00f8640b286a1f1cecc9a
Y-K-SAI-SRIKAR <saisrikaryerraguntla@gmail.com>
2025-07-23 10:01:48 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>NAME: Y. K. SAI SRIKAR</h1>
+        <h1>ID NO: 2400080069</h1>
+        <h2>BRANCH: AI&DS - HTE </h2>
+        <h2>COURSE: FRONT END DEVELOPMENT FRAMEWORKS</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
